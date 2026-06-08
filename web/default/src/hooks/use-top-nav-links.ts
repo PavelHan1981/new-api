613eea818f9385f8041d78572bdd48ead8fbf8ae
Pavel Han <windlazio@163.com>
2026-06-08 18:00:44 +0800

    去掉了主页左上角的文档链接，主要用于测试一下阿里云的镜像云服务是否工作正常

--- web/default/src/hooks/use-top-nav-links.ts
+++ web/default/src/hooks/use-top-nav-links.ts
@@ -85,14 +85,7 @@ export function useTopNavLinks(): TopNavLink[] {
     links.push({ title: t('Rankings'), href: '/rankings', requiresAuth })
   }
 
-  // Docs (supports external links)
-  if (modules?.docs !== false) {
-    if (docsLink) {
-      links.push({ title: t('Docs'), href: docsLink, external: true })
-    } else {
-      links.push({ title: t('Docs'), href: '/docs' })
-    }
-  }
+
 
   // About
   if (modules?.about !== false) {
